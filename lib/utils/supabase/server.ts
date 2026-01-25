14e4d9c6cae62edf487285e2f35fcbf7d1073437
Amy <meganar7777@gmail.com>
2026-01-24 20:48:28 -0500

    Fixed linting issues

--- lib/utils/supabase/server.ts
+++ lib/utils/supabase/server.ts
@@ -1,5 +1,12 @@
 import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+
+type CookieToSet = {
+  name: string;
+  value: string;
+  options?: Parameters<NextResponse["cookies"]["set"]>[2];
+};
 
 export function createClient() {
   const cookieStore = cookies();
@@ -12,7 +19,7 @@ export function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: CookieToSet[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
